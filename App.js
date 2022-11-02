ca402e45891743c70ca2add492f4e9d697b24c74
daisymmendoza <89948539+daisymmendoza@users.noreply.github.com>
2022-11-01 23:41:22 -0700

    Update App.js

--- App.js
+++ App.js
@@ -10,26 +10,21 @@ function App() {
 
 function LoadQuotes() {
   const [quotes, setQuotes] = useState([]);
+  const [newQuotes, setNewQuotes] = useState(false);
   
   useEffect(() => {
-    fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+    fetch("http://api.quotable.io/random")
     .then ((resp) => resp.json())
     .then ((data) => setQuotes(data))
-  }, []);
-  return (
-    <div className="quotes">
-      {quotes.map((quote) => (
-        <Quote quote={quote}></Quote>
-      ))}
-    </div>
-  );
-}
-
-function Quote(props) {
-  const {quotes} = props.quote;
+  }, [newQuotes]);
   return (
     <div className="quote">
-      <h2>{quotes}</h2>
+      <h1>"{quotes.content}"</h1>
+      <h2>-{quotes.author}</h2>
+
+      <br></br>
+
+      <button onClick={() => setNewQuotes(!newQuotes)}>New Quote</button>
     </div>
   );
 }
